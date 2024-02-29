c52e6c12ec6f837214880b923a6c5a9d4c2e2116
yahdielo <yahdielaz@gmail.com>
2024-02-28 20:10:16 -0400

    optimism sempolia test

---
+++ README.md
@@ -0,0 +1,7 @@
+# OP sempolia test
+
+this project it to deploy a simple smart contract to optimism sempolia testnet
+to help you set up your hardhat.conifg
+
+	https://docs.optimism.io/chain/networks
+
---
+++ contracts/example.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract example {
+
+	string myString = 'hello world';
+
+	functions sayHello() external returns(string myString){}
+}
